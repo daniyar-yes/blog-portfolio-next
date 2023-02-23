fa3fd98d531b5770b0aab4d02faf5dd27dd0f350
Daniyar <yeskaliyevdan@hotmail.com>
2023-02-23 01:10:38 -0800

    added pre-rendering and data fetching from fs

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,46 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <strong>Daniyar</strong>. I'm a Software Engineer and Game Developer.</p>
+        <p>Hello, I'm <strong>Daniyar</strong>. 
+          <br></br>
+          I'm a Software Engineer in Web and Game Dev.
+        </p>
         <p>
           I'm passionate about the Front End, Games and 3D, and here is the example of{' '}
           <a href="https://daniyar-yes.github.io/portfolio-3d/" target="_blank">transforming this passion into action</a>.
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
